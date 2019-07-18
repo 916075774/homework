c7f8e4b5cea74f13b81892a5ae8731d423705c06
www.916075774@qq.com <www.916075774@qq.com>
2019-07-18 17:03:57 +0800

    Add User.InviteBy

--- BLL/User.cs
+++ BLL/User.cs
@@ -7,6 +7,7 @@ namespace BLL
         public int Id { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
+        public User InviteBy { get; set; }
 
         public void Register()
         {
