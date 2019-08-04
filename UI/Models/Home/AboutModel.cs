498ae642dd60e43dc17d167d41ee8527cda91840
www.916075774@qq.com <www.916075774@qq.com>
2019-08-04 22:59:02 +0800

    一个简单的FrameWork.Mvc页面

---
+++ UI/Models/Home/AboutModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models.Register
+{
+    public class AboutModel
+    {
+        public string Greet { get; } = "hello,大帅哥";
+    }
+}
---
+++ UI/Models/Register/IntextModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models.Register
+{
+    public class IntextModel
+    {
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public bool RememberMe { get; set; }
+        //public 
+    }
+}
