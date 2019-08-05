80e4432187f92228e223a0cff9815a2e5c36a44a
www.916075774@qq.com <www.916075774@qq.com>
2019-08-05 22:43:19 +0800

    一次好像没做什么的提交

--- UI/Controllers/RegisterController.cs
+++ UI/Controllers/RegisterController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UI.Models.Register;
 
 namespace UI.Controllers
 {
     public class RegisterController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(IndexModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return View();
+        }
     }
 }
--- UI/Models/Register/IntextModel.cs
+++ UI/Models/Register/IntextModel.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace UI.Models.Register
 {
-    public class IntextModel
+    public class IndexModel
     {
+        [Required(ErrorMessage ="* 用户名不能为空")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage ="* 密码不能为空")]
         public string PassWord { get; set; }
+
         public bool RememberMe { get; set; }
         //public 
     }
